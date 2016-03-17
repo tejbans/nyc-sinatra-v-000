3bd51ef63327979cacdee40c5cbcb0f63f504fd1
tejbans <singh.tejbans@gmail.com>
2016-03-17 14:04:48 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < Sinatra::Base
 
   set :views, Proc.new { File.join(root, "../views/") }
   register Sinatra::Twitter::Bootstrap::Assets
+
+  get 'figures/new' do
+    erb :'/figures/new'
+  end
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,38 @@
 class LandmarksController < ApplicationController
 
+    
+    get '/landmarks' do
+      @landmarks = Landmark.all
+      erb :'landmarks/index'
+    end
+
+
+
+    get '/landmarks/new' do 
+      erb :'landmarks/new'
+    end
+
+    get '/landmarks/:id' do 
+      @landmark= Landmark.find(params[:id])
+      erb :'landmarks/show'
+    end
+
+    get '/landmarks/:id/edit' do 
+      @landmark= Landmark.find(params[:id])
+      erb :'landmarks/edit'
+    end
+    
+    post '/landmarks/:id' do 
+      @landmark= Landmark.find(params[:id])
+      @landmark.update(params[:landmark])
+      redirect to "/landmarks/#{@landmark.id}"
+    end
+
+    post '/landmarks' do
+      Landmark.create(params[:landmark])
+      redirect '/landmarks'
+    end
+
+    
+
 end
